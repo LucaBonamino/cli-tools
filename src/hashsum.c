72d094d8dde0723ebafac346e9312f5a7349f78a
LucaBonamino <luca.bonamino@hotmail.com>
2025-05-09 23:36:05 +0200

    readmeFile

--- src/hashsum.c
+++ src/hashsum.c
@@ -2,17 +2,36 @@
 #include <stdlib.h>
 #include <getopt.h>
 
+typedef enum {
+	SHA256 = 0,
+	SHA385,
+	SHA512,
+	SHA1,
+	POSEIDON,
+	MD5,
+} hashalg_t;
+
 typedef struct
 {
-    char *alg;
+    hashalg_t alg;
     char *text;
     char *filename;
 } arg_t;
 
+arg_t parse_args(int argc, char *argv[]);
+
 
 int main(int argc, char *argv[]){
 
-    static struct option long_options[] = {
+	arg_t args = parse_args(argc, argv);
+    printf("Argument %s", args.filename);
+
+    return EXIT_SUCCESS;
+}
+
+arg_t parse_args(int argc, char *argv[]){
+	
+	static struct option long_options[] = {
         {"alg", required_argument, NULL, 'a'},
 		{"file",	 required_argument, NULL, 'f'},
 		{ "verbose", no_argument, NULL, 'v' },
@@ -29,12 +48,10 @@ int main(int argc, char *argv[]){
 				break;
 			case '?':
 			default:
-				fprintf(stderr, "Usge: %s [--dir <directory>]\n", argv[0]);
+				fprintf(stderr, "Usge: %s [-a|$--algorithm] [-f|--file <filename>] [text]\n", argv[0]);
 				exit(EXIT_FAILURE);
 		}
 	}
-
-    printf("Argument %s", args.filename);
-
-    return EXIT_SUCCESS;
+	return args;
+	
 }
